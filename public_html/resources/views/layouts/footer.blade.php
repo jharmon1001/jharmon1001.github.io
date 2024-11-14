2b07fcf98137c5f87876abe74721314a7e2d1907
jharmon1001 <jharmon@kanje.tech>
2024-11-14 16:28:12 -0500

    Initial Commit

---
+++ public_html/resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<!-- FOOTER -->
+<footer class="footer">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-12 col-sm-12 text-center">
+                <span class="text-muted fs-11">{{ __('Copyright') }} © {{ date("Y") }} <a href="{{ config('app.url') }}">{{ config('app.name') }}</a>. {{ __('All rights reserved') }}</span>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- END FOOTER -->
